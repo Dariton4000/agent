7bb9f4557d65a6f85affd97348cd51ef93de2876
Lukas Schmelzer <lukas@familieschmelzer.de>
2025-05-11 11:50:35 +0200

    Add initial implementation for model loading and validation

---
+++ main.py
@@ -0,0 +1,20 @@
+import lmstudio as lms
+import time
+import pick
+
+
+if lms.list_loaded_models() == []:
+    print("No models loaded")
+    title = 'Please choose a model to load:'
+    options = [model.model_key for model in lms.list_downloaded_models("llm")]
+    option, index = pick.pick(options, title)
+    with lms.Client() as client:
+        model = client.llm.load_new_instance(options[index])
+else:
+    with lms.Client() as client:
+        model = client.llm.model()
+        if model.get_info().trained_for_tool_use == False:
+            print("Model is not trained for tool use."
+                  "Please be careful when using it.")
+        else:
+            print(model.get_info().identifier, "is already loaded.")
